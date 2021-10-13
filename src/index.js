1a267b55a3ea434d85765ca95561887defa7bf16
Vlad Solokha <vsolokha@gmail.com>
2021-10-13 13:24:34 -0600

    added express server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import 'dotenv/config';
+import 'dotenv/config'
+import express from 'express'
  
-console.log('Hello Node.js project.');
+const app = express()
  
-console.log(process.env.MY_SECRET);
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
+
+const userName = {firstName: 'Vlad'}
+const userCredentials = {role: 'software developer'}
+
+const user = {
+    ...userName,
+    ...userCredentials 
+}
+
+console.log(user)
+
+console.log(process.env.MY_SECRET)
